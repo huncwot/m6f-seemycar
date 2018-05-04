841f708bfe7479c85979991177cd1971ab54e781
Rafał "Huncwot" Michalski <rafal.michalski@redefine.pl>
2018-05-04 15:03:25 +0200

    fix acp language keys

--- language/pl/info_acp_main.php
+++ language/pl/info_acp_main.php
@@ -12,4 +12,5 @@ $lang = array_merge($lang, array(
     'ACP_SEEMYCAR' => 'See My Car',
     'ACP_SEEMYCAR_SETTINGS' => 'Ustawienia',
     'ACP_SEEMYCAR_EXPLAIN' => 'Autorowi tematu w wybranych forach zostanie przypisany link do tego tematu. Ten link będzie widoczny w profilu tego użytkownika i przy każdym jego poście. Autor tematu ma prawo dowolnie edytować pierwszy post tego tematu.',
-    ));
+    'SELECT_FORUM_EXPLAIN' => 'Możesz wybrać więcej niż jedno forum.',
+));
