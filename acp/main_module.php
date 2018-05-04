5396bd3d56c20cb818cba6df08be74286466ac94
Rafał "Huncwot" Michalski <rafal.michalski@redefine.pl>
2018-05-04 15:12:34 +0200

    new config variable

--- acp/main_module.php
+++ acp/main_module.php
@@ -4,6 +4,7 @@ namespace huncwot\seemycar\acp;
 
 use huncwot\seemycar\service\main;
 use phpbb\config\config;
+use phpbb\language\language;
 use phpbb\request\request;
 use phpbb\template\template;
 use phpbb\user;
@@ -39,17 +40,27 @@ class main_module
         /* @var $main main */
         $main = $phpbb_container->get('huncwot.seemycar.service.main');
 
-        $forums_ids = $main->get_forums_ids();
+        $current_forums_ids = $main->get_forums_ids();
+        $current_update_frequency = (int) $config['seemycar_update'];
 
-        if ($request->is_set_post('forum')) {
+        if ($request->is_set_post('submit')) {
             if (false === check_form_key($form_name)) {
                 trigger_error($user->lang['FORM_INVALID'].adm_back_link($this->u_action), E_USER_WARNING);
             }
 
             $new_forums_ids = $request->variable('forum', array(0));
+            $new_frequency = $request->variable('frequency', 14400);
+            $update_now = $request->variable('update_now', 0);
 
-            if ($forums_ids !== $new_forums_ids) {
+            if ($current_forums_ids !== $new_forums_ids) {
                 $config->set('seemycar_data', json_encode($new_forums_ids));
+            }
+
+            if ($current_update_frequency !== $new_frequency) {
+                $config->set('seemycar_update', $new_frequency);
+            }
+
+            if (1 === $update_now) {
                 $main->update_links();
             }
 
@@ -58,7 +69,29 @@ class main_module
 
         $template->assign_vars(array(
             'U_ACTION' => $this->u_action,
-            'S_OPTIONS' => make_forum_select($forums_ids),
+            'S_FORUM_OPTIONS' => make_forum_select($current_forums_ids),
+            'S_FREQUENCY_OPTIONS' => $this->make_frequency_select($current_update_frequency),
         ));
+    }
+
+    /**
+     * @global ContainerInterface $phpbb_container
+     * @param integer $update_frequency
+     * @return string
+     */
+    function make_frequency_select($update_frequency)
+    {
+        global $phpbb_container;
+
+        /* @var $language language */
+        $language = $phpbb_container->get('language');
+
+        $options = '';
+        foreach (array(1 => 3600, 2 => 7200, 3 => 10800, 4 => 14400, 6 => 21600, 12 => 43200, 24 => 86400) as $hour => $seconds) {
+            $selected = $update_frequency === $seconds ? ' selected="selected"' : '';
+            $options .= "<option value=\"{$seconds}\"{$selected}>{$language->lang_array('HOURLY_INTERVALS', array($hour))}</option>";
+        }
+
+        return $options;
     }
 }
--- language/en/info_acp_main.php
+++ language/en/info_acp_main.php
@@ -13,4 +13,10 @@ $lang = array_merge($lang, array(
     'ACP_SEEMYCAR_SETTINGS' => 'Settings',
     'ACP_SEEMYCAR_EXPLAIN' => 'The author of the topic in selected forums will be assigned with a link to that topic. This link will be visible in this user profile and next to each his post. The author of the topic has the right to freely edit that topic\'s first post.',
     'SELECT_FORUM_EXPLAIN' => 'You are able to select more than one forum.',
+    'LINKS_UPDATE_FREQUENCY' => 'Links update frequency',
+    'HOURLY_INTERVALS' => array(
+        1 => 'every hour',
+        2 => 'every %d hours',
+    ),
+    'UPDATE_NOW' => 'Update now',
 ));
